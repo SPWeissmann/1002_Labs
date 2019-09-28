38af040bea39ad1880bebb9ae4aacb8fd2c668e7
Sam <sammuel.p.weissmann@gmail.com>
2019-09-27 23:36:15 -0400

    wrote cylinder module
    Wrote and tested surface area and volume functions for cylinder module.

--- Lab4/cylinder.py
+++ Lab4/cylinder.py
@@ -5,15 +5,18 @@ Created on Fri Sep 27 22:37:51 2019
 
 @author: sam
 """
+import circle
+
 
 def surface(height, radius):
-    
-    
-    pass
-
-
+    #surface area  = (2 * pi * radius * height) + (2 * pi * r^2)
+    #              = (base circumference * height) + (2 * base area)
+    #              = area of sides + area of two ends
+    ends_area =  2 * circle.area(radius)
+    sides_area = circle.circumference(radius) * height
+    return ends_area + sides_area
 
 def volume(height, radius):
-    
-    
-    pass
+    #volume = base * height
+    base = circle.area(radius)
+    return base * height
