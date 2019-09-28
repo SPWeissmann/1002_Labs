488992534af363428e7ca30c2a583097838457d8
Sam <sammuel.p.weissmann@gmail.com>
2019-09-27 23:37:34 -0400

    Wrote sphere module
    Wrote and tested surface area and volume functions for sphere module

--- Lab4/sphere.py
+++ Lab4/sphere.py
@@ -5,15 +5,13 @@ Created on Fri Sep 27 22:37:42 2019
 
 @author: sam
 """
-
-def surface(height, radius):
-    
-    
-    pass
+import math
 
 
+def surface(radius):
+    #surface area = 4 * pi * r^2
+    return (4*math.pi*(radius**2))
 
-def volume(height, radius):
-    
-    
-    pass
+def volume(radius):
+    # volume = (4/3) * pi * r^3
+    return (4*math.pi*(radius**3)) / 3
