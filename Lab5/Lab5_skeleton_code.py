72f7360795f068de69b2fbf6ca32060ca23c6553
Sam <sammuel.p.weissmann@gmail.com>
2019-10-04 11:45:06 -0400

    wrote class solution for hamming_distance

--- Lab5/Lab5_skeleton_code.py
+++ Lab5/Lab5_skeleton_code.py
@@ -13,21 +13,41 @@ def isPalindrome():
     pass
 
 
-def hammingDistance():
+def hamming_distance(s1,s2):
     '''Takes in two strings of equal length, and returns the hamming
     distance between them'''
-    distance = 0
+    count = 0
+    
+    for i in range(len(s1)):
+        if s1[i] != s2[i]:
+            count = count + 1
+            
+    return count
 
-    #code goes here
-
-    return distance
-
+def do_stuff(str1, int1):
+    '''Example of a function where the order of the arguments matter when 
+    calling it.
+    
+    Takes an integer and a string, and prints the string to 
+    the user and prints the value of the integer + 1 to the user'''
+    
+    print(str1)
+    print(int1 + 1)
+    
+    return True
+        
+    
 def main():
     '''Prompts the user to choose whether to check a palindrome
     or the hamming distance, and then computes tha answer using
     the functions defined above'''
-
-    pass
+    
+    
+    input1 = input("please type something")
+    input2 = input("please type something")
+    
+    hamming_distance(input1, input2)
+    
     #end of main function
 
 #What do we need to add here in order to make file run when executed?
